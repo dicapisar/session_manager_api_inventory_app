dc8b77c2b046f16dc4b9177fe83b5473e51fe579
dicapisar <dicapisar@gmail.com>
2022-05-04 22:46:36 -0500

    Initialization proyect

---
+++ src/main/java/com/dicapisar/sessionManager/SessionManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.dicapisar.sessionManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessionManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicapisar/sessionManager/SessionManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicapisar.sessionManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
